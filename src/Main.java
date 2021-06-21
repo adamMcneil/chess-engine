5e636b70273962b5d9c48f82435ba774ad74be9b
Adam McNeil <adamwmcneil@gmail.com>
2021-06-20 23:32:57 -0500

    fix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args){
         Board board = new Board();
         board.printBoard();
-        board.printBoard();
     }
     //function checkPawnMove(char[8][8] board, int[2] location) outputs an array of all locations a pawn can move
 }
