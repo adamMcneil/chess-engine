d8281620c02adc5c595d89bed4a6b6f2cb20cec2
adamwm4 <adamwm4@illinois.edu>
2025-03-25 10:42:53 -0500

    added test for each piece

--- src/test/java/cheekykoala/BoardTest.java
+++ src/test/java/cheekykoala/BoardTest.java
@@ -1,54 +1,100 @@
 package cheekykoala;
 
-import cheekykoala.pieces.Pawn;
+import cheekykoala.pieces.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
     @Test
-    void testAddPiece() {
-        Board board = new Board();
-        Position position = new Position(0, 0);
-        Pawn pawn = new Pawn(Color.w, position);
-        board.addPiece(position, pawn);
-        assertEquals(pawn.getLetter(), board.getPieceAt(position).getLetter());
-    }
-
-    @Test
-    void testWhiteInCheckmate() {
-        Board board = new Board();
-        board.importBoard("8/8/8/8/8/8/8/RK6 b - - 0 1");
-        assertFalse(board.whiteInCheckmate());
-        board.importBoard("8/8/8/8/8/8/8/k7 b - - 0 1");
-        assertFalse(board.whiteInCheckmate());
-    }
-
-    @Test
-    void testBlackInCheckmate() {
-        Board board = new Board();
-        board.importBoard("8/8/8/8/8/8/8/RK6 w - - 0 1");
-        assertFalse(board.blackInCheckmate());
-        board.importBoard("8/8/8/8/8/8/8/k7 w - - 0 1");
-        assertTrue(board.blackInCheckmate());
-    }
-
-    @Test
-    void testCheckStalemate() {
-        Board board = new Board();
-        board.importBoard("8/8/8/8/8/8/8/RK6 w - - 0 1");
-        assertFalse(board.checkStalemate(Color.w));
-        board.importBoard("8/8/8/8/8/8/8/k7 w - - 0 1");
-        assertTrue(board.checkStalemate(Color.b));
-    }
-
-    @Test
-    void testImportBoard() {
+    void testStartMoves() {
         Board board = new Board();
         board.importBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-        assertEquals('r', board.getPieceAt(new Position(0, 0)).getLetter());
-        assertEquals('R', board.getPieceAt(new Position(0, 7)).getLetter());
-        assertEquals('k', board.getPieceAt(new Position(4, 0)).getLetter());
-        assertEquals('K', board.getPieceAt(new Position(4, 7)).getLetter());
+        int whiteMoves = board.getAllMoves(Color.w).size();
+        int blackMoves = board.getAllMoves(Color.b).size();
+        assertEquals(20, whiteMoves);
+        assertEquals(20, blackMoves);
     }
+
+    @Test
+    void testRookMoves() {
+        Board board = new Board();
+        board.importBoard("8/8/P7/8/R4p2/8/8/8 w - - 0 1");
+        Piece rook = board.get(0,4 );
+        assertTrue(rook.isRook());
+        assertSame(rook.getColor(), Color.w);
+        int moves = rook.getMoves(board).size();
+        assertEquals(9, moves);
+    }
+
+    @Test
+    void testBishopMoves() {
+        Board board = new Board();
+        board.importBoard("8/8/8/3b4/8/8/8/8 b - - 0 1");
+        Piece bishop = board.get(3, 3);
+        assertTrue(bishop.isBishop());
+        assertSame(bishop.getColor(), Color.b);
+        int moves = bishop.getMoves(board).size();
+        assertEquals(13, moves);
+    }
+
+    @Test
+    void testKnightMoves() {
+        Board board = new Board();
+        board.importBoard("8/8/8/8/3N4/8/8/8 w - - 0 1");
+        Piece knight = board.get(3, 4);
+        assertTrue(knight.isKnight());
+        assertSame(knight.getColor(), Color.w);
+        int moves = knight.getMoves(board).size();
+        assertEquals(8, moves);
+    }
+
+    @Test
+    void testQueenMoves() {
+        Board board = new Board();
+        board.importBoard("8/8/8/8/4Q3/8/8/8 w - - 0 1");
+        Piece queen = board.get(4, 4);
+        assertTrue(queen.isQueen());
+        assertSame(queen.getColor(), Color.w);
+        int moves = queen.getMoves(board).size();
+        assertEquals(27, moves);
+    }
+
+    @Test
+    void testKingMoves() {
+        Board board = new Board();
+        board.importBoard("8/8/8/8/4K3/8/8/8 w - - 0 1");
+        Piece king = board.get(4, 4);
+        assertTrue(king.isKing());
+        assertSame(king.getColor(), Color.w);
+        int moves = king.getMoves(board).size();
+        assertEquals(8, moves);
+    }
+
+    @Test
+    void testPawnMoves() {
+        Board board = new Board();
+        board.importBoard("8/8/8/8/3P4/8/8/8 w - - 0 1");
+        Piece pawn = board.get(3, 4);
+        assertTrue(pawn.isPawn());
+        assertSame(pawn.getColor(), Color.w);
+        int moves = pawn.getMoves(board).size();
+        assertEquals(1, moves); // Assuming no double move or captures available
+    }
+
+    @Test
+    void testPromotionMoves() {
+        Board board = new Board();
+        board.importBoard("8/P7/8/8/8/8/p7/8 w - - 0 1");
+        Piece whitePawn = board.get(0, 1);
+        Piece blackPawn = board.get(0, 6);
+        assertTrue(whitePawn.isPawn());
+        assertTrue(blackPawn.isPawn());
+        int whiteMoves = whitePawn.getMoves(board).size();
+        int blackMoves = blackPawn.getMoves(board).size();
+        assertEquals(4, whiteMoves);
+        assertEquals(4, blackMoves);
+    }
+
+
 }
